d181a5729879564bcbcf66074343dad0abc95b3e
weber lucas <weber13lucas@gmail.com>
2025-04-18 12:16:23 -0300

    test(api):alterando caminho do mocha

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -1,3 +1,4 @@
 import 'cypress-plugin-api' // feedback visual ao rodar um teste de api!
 import 'cypress-mochawesome-reporter/register'; // Responsável por gerar relatório
 import './commands';//Arquivo commands 
+import 'cypress-split';
