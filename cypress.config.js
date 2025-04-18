744959f04c6bbb9513111380be301474af8635d5
weber lucas <weber13lucas@gmail.com>
2025-04-17 21:55:30 -0300

    test(api):removendo configuração do commands no cypress config

--- cypress.config.js
+++ cypress.config.js
@@ -6,10 +6,9 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // event listeners podem ser definidos aqui, se precisar
     },
-    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
-    supportFile: 'cypress/support/e2e.js',
-    experimentalRunAllSpecs: true,//permite que ao rodar npx cypress open habilite o botão de rodar todos os testes 
 
+    experimentalRunAllSpecs: true,//permite que ao rodar npx cypress open habilite o botão de rodar todos os testes 
+    //este botão fica ao lado de componet specs
   },
   reporter: 'mochawesome',
   reporterOptions: {
